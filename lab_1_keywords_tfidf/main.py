6a627e91dc19917de3a8064fc9f5eca4b626bcfc
milya <dzami.biz@gmail.com>
2025-09-22 19:40:31 +0300

    please

--- lab_1_keywords_tfidf/main.py
+++ lab_1_keywords_tfidf/main.py
@@ -5,8 +5,10 @@ Extract keywords based on frequency related metrics
 """
 
 # pylint:disable=unused-argument
-from typing import Any
 import math
+
+from typing import Any
+
 
 
 def check_list(user_input: Any, elements_type: type, can_be_empty: bool) -> bool:
