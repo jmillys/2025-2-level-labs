1164720312d01408b09c17dfc71addc58a4cfa8f
milya <dzami.biz@gmail.com>
2025-10-15 19:28:16 +0300

    sdofufdhshdsgdiodssaiogdioidsgfso;dfsolkjldfslkjdoielk

--- lab_2_spellcheck/start.py
+++ lab_2_spellcheck/start.py
@@ -3,7 +3,10 @@ Spellcheck starter
 """
 
 # pylint:disable=unused-variable, duplicate-code, too-many-locals
-from lab_1_keywords_tfidf.main import clean_and_tokenize, remove_stop_words
+from lab_1_keywords_tfidf.main import (
+    clean_and_tokenize, 
+    remove_stop_words
+)
 from lab_2_spellcheck.main import (
     build_vocabulary,
     calculate_distance,
@@ -30,13 +33,26 @@ def main() -> None:
     ):
         sentences = [f.read() for f in (f1, f2, f3, f4, f5)]
     tokens = clean_and_tokenize(text) or []
-    tokens_without_stop_words = remove_stop_words(tokens, stop_words) or []
-    vocabulary = build_vocabulary(tokens_without_stop_words) or {}
-    out_of_vocab_words = find_out_of_vocab_words(tokens_without_stop_words, vocabulary) or []
-    distance_dict = {}
-    for word in out_of_vocab_words:
-        distance_dict[word] = calculate_distance(word, vocabulary, 'jaccard', None) or {}
-    result = distance_dict
+    tokens_without_stopwords = remove_stop_words(tokens, stop_words) or []
+    vocabulary = build_vocabulary(tokens_without_stopwords) or {}
+    all_sentence_tokens = []
+    for sentence in sentences:
+        sentence_tokens = clean_and_tokenize(sentence) or []
+        sentence_tokens_without_stop_words = remove_stop_words(sentence_tokens, stop_words) or []
+        all_sentence_tokens.extend(sentence_tokens_without_stop_words)
+    wrong_words = find_out_of_vocab_words(all_sentence_tokens, vocabulary) or []
+    print(wrong_words)
+    alphabet = [chr(i) for i in range(1072, 1104)]
+    all_results = {}
+    for wrong_word in wrong_words:
+        print(f"\nИсправление для '{wrong_word}':")
+        jaccard_correction = find_correct_word(wrong_word, vocabulary,
+                                               'jaccard', alphabet) or {}
+        print(f"  Jaccard: {jaccard_correction}")
+        all_results[wrong_word] = {
+            'jaccard': jaccard_correction
+        }
+    result = all_results
     assert result, "Result is None"
 
 
