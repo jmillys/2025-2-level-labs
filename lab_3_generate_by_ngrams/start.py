b82e4aa4490019c6b65a0c610fa676c34f265c64
milya <dzami.biz@gmail.com>
2025-11-12 21:17:48 +0300

    i hate this

--- lab_3_generate_by_ngrams/start.py
+++ lab_3_generate_by_ngrams/start.py
@@ -3,7 +3,11 @@ Generation by NGrams starter
 """
 
 # pylint:disable=unused-import, unused-variable
-
+from lab_3_generate_by_ngrams.main import (
+    GreedyTextGenerator,
+    NGramLanguageModel,
+    TextProcessor,
+)
 
 def main() -> None:
     """
@@ -14,6 +18,11 @@ def main() -> None:
     with open("./assets/Harry_Potter.txt", "r", encoding="utf-8") as text_file:
         text = text_file.read()
     result = None
+    processor = TextProcessor('_')
+    encoded_text = processor.encode(text) or ()
+    print(encoded_text)
+    decoded_text = processor.decode(encoded_text) or ()
+    print(decoded_text)
     assert result
 
 
