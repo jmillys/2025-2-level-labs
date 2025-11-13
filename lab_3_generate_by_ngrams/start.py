2982aebb78c88f2b200715d122922c8eedddb32c
milya <dzami.biz@gmail.com>
2025-11-13 21:08:26 +0300

    shdhfidfgidfgdikdjdf

--- lab_3_generate_by_ngrams/start.py
+++ lab_3_generate_by_ngrams/start.py
@@ -17,12 +17,17 @@ def main() -> None:
     """
     with open("./assets/Harry_Potter.txt", "r", encoding="utf-8") as text_file:
         text = text_file.read()
-    result = None
     processor = TextProcessor('_')
     encoded_text = processor.encode(text) or ()
     print(encoded_text)
     decoded_text = processor.decode(encoded_text) or ()
     print(decoded_text)
+    model = NGramLanguageModel(encoded_text, 7)
+    model.build()
+    greedy_generator = GreedyTextGenerator(model, processor)
+    greedy_text = greedy_generator.run(51, 'Vernon')
+    print(greedy_text)
+    result = greedy_text
     assert result
 
 
